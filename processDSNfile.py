f8b658904d3a874dff1b057f0c0640f7284b8508
ZacApelt <zacapelt4@gmail.com>
2025-08-16 11:12:51 +1000

    single layer autorouter, organised dsn and ses file

--- processDSNfile.py
+++ processDSNfile.py
@@ -5,8 +5,18 @@ nets = []
 
 class Pad:
     def __init__(self, name, ID, position, shape, outline, layer):
+        '''
+        name [str]: name of the pad
+        ID [int]: ID of the pad
+        position [tuple]: (x, y) position of the pad
+        shape [str]: shape of the pad (e.g., "circle", "polygon")
+        outline [list]: outline of the pad
+                        This is a list of points (polygon: [x0 y0 x1 y1 ...], circle: [diamerter]) 
+                        that are relative to the pad's position
+        layer [int]: layer of the pad (1 is top, 2 is bottom, 21, 22,... are interlayers)
+        '''
         self.name = name
-        self.ID = ID
+        self.ID = int(ID)
         self.position = position
         self.shape = shape
         self.outline = outline
@@ -18,6 +28,12 @@ class Pad:
     
     def getName(self):
         return self.name
+    
+    def getID(self):
+        return self.ID
+    
+    def getPosition(self):
+        return self.position
 
     def addShape(self, shape, outline, layer):
         self.shape = shape
@@ -25,22 +41,32 @@ class Pad:
         self.layer = layer
 
 class Net:
-    def __init__(self, name, pins):
+    def __init__(self, name, pads):
+        '''
+        name [str]: name of the net
+        pads [list]: list of pad objects connected to the net
+        '''
         self.name = name
-        # pins is an array
-        self.pins = pins
+        self.pads = pads
         self.wires = [] # will contain tuples of coordinates for wire segments in the net, and layer number
         self.vias = []
 
     def addWireSegment(self, start, end, layer):
+        '''
+        A wire segment is straight line between 2 points start:(x1, y1) and end:(x2, y2)
+        '''
         self.wires.append((start, end, layer))
     
     def addVia(self, position):
         self.vias.append(position)
 
+    def getPadsInNet(self):
+        return self.pads
+
 
 
 def processDSNfile(file_name):
+    # function to populate the pads and nets lists from a DSN file
 
     with open(file_name, "r") as file:
         content = file.read()
@@ -54,12 +80,6 @@ def processDSNfile(file_name):
     for i in range(len(lines)):
         line = lines[i]
 
-        # count leading spaces
-        '''num_spaces = len(line) - len(line.lstrip(' '))
-        if num_spaces % 2 == 1:
-            num_spaces -= 1
-        spaces.append(num_spaces)
-        '''
         # conditioned line to remove the first and last bracket
         line = line.strip()[1:-1]
 
@@ -100,17 +120,29 @@ def processDSNfile(file_name):
                     pad_i.addShape(shape_type, shape_outline, shape_layer)
         
         if "pins" in line:
+            # pins_in_net is a list of the IDs of the pads
             pins_in_net = line.split(" ")[1:]
             for j in range(len(pins_in_net)):
                 pins_in_net[j] = int(pins_in_net[j].split("-")[1])
-
+            
+            # pads_in_net is a list of the pad objects in the net
+            #print(pins_in_net, pads)
+            pads_in_net = []
+            for pin in pins_in_net:
+                for pad in pads:
+                    #print(f"padid: {type(pad.getID())}, {type(pin)}")
+                    if pad.getID() == pin:
+                        pads_in_net.append(pad)
+            #print(f"pads_in_net: {pads_in_net}")
             # net name from previous line
             net_name = lines[i - 1].strip().split(" ")[1]
 
-            nets.append(Net(net_name, pins_in_net))
+            nets.append(Net(net_name, pads_in_net))
 
 
 def processSESfile(file_name):
+    # function to create a .ses file from the new wires and vias
+
     # write to a new .ses file
     with open(file_name, "w") as file:
         # header
@@ -118,7 +150,7 @@ def processSESfile(file_name):
 
         for net in nets:
             file.write(f"\t\t\t(net {net.name}\n")
-            print(f"wire segments: {net.wires}")
+            #print(f"wire segments: {net.wires}")
             #print(len(net.wires))
             for i in range(len(net.wires)):
                 #print(i)
@@ -135,16 +167,35 @@ def processSESfile(file_name):
 
         file.write(f"\t\t)\n\t)\n)\n")
 
+def convertCoordinates(coords):
+    # coords is a tuple of (x, y)
+    x, y = coords
+    return (x * 1000, y * 1000)
 
-processDSNfile("Autorouter_PCB_cs-hackathon_2_2025-08-15.dsn")
+def veryBasicRoute():
+    # route the net called "LED1_1"
 
-nets[0].addWireSegment((0, 0), (100000, 100000), 1)
-nets[0].addWireSegment((100000, 100000), (200000, 100000), 2)
-nets[1].addWireSegment((0, 0), (100000, 50000), 1)
-nets[1].addVia((50000, 25000))
-nets[1].addVia((75000, 37500))
+    pads_in_net = []
+    for net in nets:
+        #if net.name == "LED1_1":
+        pads_in_net = net.getPadsInNet()
+    
+        for i in range(len(pads_in_net) - 1):
+            #print(f"net: {net.name}, {pad.getPosition()}")
+            pad1_pos = pads_in_net[i].getPosition()
+            pad2_pos = pads_in_net[i + 1].getPosition()
 
-processSESfile("Autorouter_PCB_cs-hackathon_2_2025-08-15.ses")
+            pad1_pos = convertCoordinates(pad1_pos)
+            pad2_pos = convertCoordinates(pad2_pos)
+
+            print(f"net: {net.name}, {pad1_pos} to {pad2_pos}")
+            net.addWireSegment(pad1_pos, pad2_pos, 1)
+
+processDSNfile("DSN/basic1layerRoute.dsn")
+
+veryBasicRoute()
+
+processSESfile("SES/basic1layerRoute.ses")
 
 '''
 for pad_i in pads:
